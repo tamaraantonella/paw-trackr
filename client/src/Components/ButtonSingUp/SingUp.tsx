f9158df927ecbf0e8434c183b74f2760aff5e232
jesusre890 <jesusre890@gmail.com>
2023-09-22 11:11:19 -0300

    chore(changes made): changes made

--- client/src/Components/ButtonSingUp/SingUp.tsx
+++ client/src/Components/ButtonSingUp/SingUp.tsx
@@ -4,7 +4,7 @@ const SingUp = () => {
   return (
     <div className="my-10">
       <button className="botonSing flex text-center justify-center items-center">
-        <p className='singUp text-xl'>SING UP</p>
+        <p className='singUp text-xl'>SIGN UP</p>
         <div className="arrow-wrapper">
           <div className="arrow"></div>
         </div>
--- client/src/Views/Landing/Landing.tsx
+++ client/src/Views/Landing/Landing.tsx
@@ -4,15 +4,13 @@ import SingUp from "../../Components/ButtonSingUp/SingUp";
 
 const Landing = () => {
   return (
-    <div className="landing flex justify-center text-center items-center mt-36">
-      <div className="flex flex-col items-center">
-        <img src={logo} alt="logo blue grey" className=" w-96" />
-        <h2 className="text text-3xl">Welcome!</h2>
-        <SingUp />
-        <div className="flex gap-2">
-          <p className="text font-medium">Already have an account?</p>
-          <button className="login font-bold hover:text-blue-800">Login</button>
-        </div>
+    <div className="landing flex flex-col justify-center text-center items-center mt-36">
+      <img src={logo} alt="logo blue grey" className="w-96" />
+      <h2 className="text text-3xl">Welcome!</h2>
+      <SingUp />
+      <div className="flex gap-2">
+        <p className="text font-medium">Already have an account?</p>
+        <button className="login font-bold hover:text-blue-800">Login</button>
       </div>
     </div>
   );
